7893181b03eea9fe4b00791fa9f2afa9162115f9
Ertibus <47594900+Ertibus@users.noreply.github.com>
2021-05-30 13:41:18 +0300

    Added PostConstruct and PreDestroy methods

--- src/main/java/lt/viko/eif/savaitgalis/gpd/CovidApiApplication.java
+++ src/main/java/lt/viko/eif/savaitgalis/gpd/CovidApiApplication.java
@@ -3,6 +3,9 @@ package lt.viko.eif.savaitgalis.gpd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * Spring Boot launcher.
  *
@@ -14,6 +17,16 @@ public class CovidApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CovidApiApplication.class, args);
+	}
+
+	@PostConstruct
+	private void postConstruct() {
+		System.out.println("Startup configuration");
+	}
+
+	@PreDestroy
+	private void preDestroy() {
+		System.out.println("Shutdown configuration");
 	}
 
 }
